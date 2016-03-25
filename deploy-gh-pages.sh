eb6e1e91eb43257ffda00c3a055cba6d5106f5fd
Daniel Sont <dan.sont@gmail.com>
2016-03-25 13:19:01 +0900

    gh pages script again

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -3,7 +3,13 @@
 DOMAIN="devtrax.io"
 DISTFOLDER="public"
 
+echo 'Running Hugo'
+hugo --theme="hugo-agency-theme"
+
+echo 'Ensuring CNAME is set'
 echo $DOMAIN > "$DISTFOLDER/CNAME"
 git add "$DISTFOLDER/CNAME"
 git commit -m "Adding CNAME"
+
+echo "Pushing to Gh Pages @ ${DOMAIN}"
 git subtree push --prefix "$DISTFOLDER" origin gh-pages
